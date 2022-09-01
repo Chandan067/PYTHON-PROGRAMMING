d3b11cdd416485eda2155590cf7065e81e27e3aa
Chandan067 <112244663+Chandan067@users.noreply.github.com>
2022-09-01 11:31:46 +0530

    Add files via upload

---
+++ pattern programs.py
@@ -0,0 +1,7 @@
+row=int(input("enter the no of rows :"))
+column=int(input("enter the no of columns :"))
+for i in range(1,row+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()    
+

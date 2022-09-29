77017671014fa6dfb1bdfae343736475b91adaa7
Chandan067 <112244663+Chandan067@users.noreply.github.com>
2022-09-29 11:28:56 +0530

    Add files via upload

---
+++ equilateral triangle 7th 3rd day.py
@@ -0,0 +1,16 @@
+def triangle(n):
+    k=n-1
+    for i in range(0,n+1):
+        for j in range(0,k+1):
+            print(end=" ")
+        k=k-1
+        for j in range(i,0,-1):
+            print(j,end=" ")
+        print()
+n=float(input("enter the number of rows :"))
+if(n<=0 or int(n)!=n):
+    print("INVALID INPUT")
+else:
+    triangle(int(n))
+        
+
